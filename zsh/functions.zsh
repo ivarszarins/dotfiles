0bc96c56836248e2389bf127ea9f8ad8d231c4b0
Ivars Zarins <ivars.zarins@gmail.com>
2017-02-24 12:00:34 +0100

    cleaned up functions

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -28,32 +28,6 @@ function f() {
     find . -name "$1"
 }
 
-function ng-stop() {
-    sudo launchctl stop homebrew.mxcl.nginx
-}
-
-function ng-start() {
-    sudo launchctl start homebrew.mxcl.nginx
-}
-function ng-restart() {
-     sudo launchctl start homebrew.mxcl.nginx
-}
-
-function dns-restart() {
-    sudo launchctl stop homebrew.mxcl.dnsmasq
-    sudo launchctl start homebrew.mxcl.dnsmasq
-}
-
-
-# Start an HTTP server from a directory, optionally specifying the port
-function server() {
-    local port="${1:-8000}"
-    open "http://localhost:${port}/"
-    # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
-    # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
-    python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] = value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
-}
-
 # take this repo and copy it to somewhere else minus the .git stuff.
 function gitexport(){
     mkdir -p "$1"
@@ -66,11 +40,6 @@ function gz() {
     cat "$1" | wc -c
     echo "gzipped size (bytes): "
     gzip -c "$1" | wc -c
-}
-
-# All the dig info
-function digga() {
-    dig +nocmd "$1" any +multiline +noall +answer
 }
 
 # Escape UTF-8 characters into their 3-byte format
@@ -106,12 +75,6 @@ function extract() {
     else
         echo "'$1' is not a valid file"
     fi
-}
-
-function scpp() {
-    scp "$1" nicknisi@nicknisi.com:/var/www/nicknisi.com/public_html/share;
-    echo "http://nicknisi.com/share/$1" | pbcopy;
-    echo "Copied to clipboard: http://nicknisi.com/share/$1"
 }
 
 # syntax highlight the contents of a file or the clipboard and place the result on the clipboard
